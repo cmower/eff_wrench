71cf2133a7a5a6a1b7a2bafadc83ad91e0abff85
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-04-17 20:11:54 +0200

    Update setup.py

--- eff_wrench/setup.py
+++ eff_wrench/setup.py
@@ -1,26 +1,23 @@
 from setuptools import setup
 
-package_name = 'eff_wrench'
+package_name = "eff_wrench"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="1.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools", "numpy"],
     zip_safe=True,
-    maintainer='cm22',
-    maintainer_email='christopher.mower@kcl.ac.uk',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="Christopher E. Mower",
+    maintainer_email="christopher.mower@kcl.ac.uk",
+    description="ROS 2 node for estimating end-effector wrench.",
+    license="GNU General Public License v3.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'estimate_wrench = eff_wrench.estimate_wrench:main'
-        ],
+        "console_scripts": ["estimate_wrench = eff_wrench.estimate_wrench:main"],
     },
 )
